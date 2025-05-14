0c490036dc7b9080c90d88afac30af2ba39583c7
resorgatto <renatosorgatto@gmail.com>
2025-05-14 17:54:53 -0300

    feat: add convertion to TXT, HTML, but PDF, DOCX are not working

--- src/window.py
+++ src/window.py
@@ -16,7 +16,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
+import os
+import datetime
+from pathlib import Path
 from gi.repository import Adw
 from gi.repository import Gtk, Gio, GLib
 import subprocess
@@ -27,6 +29,8 @@ class DocmorphWindow(Adw.ApplicationWindow):
 
     label = Gtk.Template.Child()
     selected_item = ""
+    selectconvert = Gtk.Template.Child()
+    selected_file = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -39,34 +43,38 @@ class DocmorphWindow(Adw.ApplicationWindow):
         convert_action.connect('activate', self.button_convert)
         self.add_action(convert_action)
 
-
-
     def open_file(self, action, _):
-        self._native = Gtk.FileChooserNative(
-        title="Open File",
-        transient_for=self,
-        action=Gtk.FileChooserAction.OPEN
-        )
-        self._native.connect('response', self.on_open_response)
-        self._native.show()
+        dialog = Gtk.FileDialog()
+        dialog.open()
+        
 
 
-    def on_open_response(self, dialog, response, _):
+    def on_open_response(self, dialog, response):
         if response == Gtk.ResponseType.ACCEPT:
-            self.open_file(dialog.get_file())
             self.selected_file = dialog.get_file()
-
+            file_path = self.selected_file.get_path()
+            print(f"Arquivo selecionado: {file_path}")
         self._native = None
 
 
 
-    def button_convert(self, action, state):
+    def button_convert(self, action, param):
+        if not self.selected_file:
+            print("Selecione um arquivo primeiro!")
+            return
+
+        input_file = self.selected_file.get_path()
+
+        # Obter o valor selecionado do ComboRow
+        file_type = self.selectconvert.get_selected_item().get_string().lower()
+
+        # Determina o nome/saída dependendo do tipo escolhido
+        output_file = f"documento_convertido.{file_type}"
+
         try:
-            print(state)
-            subprocess.run(["pandoc", "-o", "documento.pdf", state])
-            print("Conversão concluída com sucesso.")
+            subprocess.run(["pandoc", input_file, "-o", output_file], check=True)
+            print(f"Convertido com sucesso para: {output_file}")
+        except subprocess.CalledProcessError as e:
+            print(f"Erro Pandoc: {e}")
         except Exception as e:
-            print(f"Erro durante a conversão: {e}")
-
-
-
+            print(f"Erro inesperado: {e}")
